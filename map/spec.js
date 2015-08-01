e1284599e536b14aa21dfc0b1ac9d36d3e823af8
Samuel Ytterbrink <neppord@gmail.com>
2015-08-01 19:50:47 +0200

    Move simple map example from systemtests into unittests.

--- map/spec.js
+++ map/spec.js
@@ -1,23 +1,43 @@
-/*global describe it*/
+/*global describe it beforeEach*/
 var h = require('highland')
 var map = require('./')
 var parse = require('../parse')
 var should_render = require('../spec_helpers/should_render')
 
 describe('map', function () {
+  var html = '<li></li>'
+  var template
+
+  beforeEach(function () {
+    var tokens = parse()
+    template = map(tokens, id)
+    tokens.end(html)
+  })
+
   it('takes data in', function () {
     h().pipe(map(h(), id))
   })
-  it('renders tokens', function (done) {
-    var html = '<html></html>'
-    var tokens = parse()
-    tokens.end(html)
-    var template = map(tokens, id)
-
+  it('renders zero', function (done) {
+    template.end()
+    should_render(done, template, '')
+  })
+  it('renders one', function (done) {
     template.write({})
     template.end()
-
     should_render(done, template, html)
+  })
+  it('renders many ', function (done) {
+    template.write({})
+    template.write({})
+    template.end()
+    should_render(done, template, html + html)
+  })
+
+  it('gives the data to the render function', function (done) {
+    template.write({})
+    template.write({})
+    template.end()
+    should_render(done, template, html + html)
   })
 })
 
