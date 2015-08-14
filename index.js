df91ca03c242fcf088aae6566f9b1c6d5c015628
Samuel Ytterbrink <neppord@gmail.com>
2015-08-14 18:25:00 +0200

    Use through instead of pipe.

--- index.js
+++ index.js
@@ -16,30 +16,30 @@ var verify_stream = require('./verify')
 
 function mixin (self) {
   self.verify = function verify () {
-    return mixin(self.pipe(verify_stream()))
+    return mixin(self.through(verify_stream()))
   }
   self.find = function find (selector) {
-    return mixin(self.pipe(find_stream(selector)))
+    return mixin(self.through(find_stream(selector)))
   }
   self.render = function render () {
-    return mixin(self.pipe(render_stream()))
+    return mixin(self.through(render_stream()))
   }
   self.set = function set (selector, stream) {
-    return mixin(self.pipe(set_stream.outer(selector, stream)))
+    return mixin(self.through(set_stream.outer(selector, stream)))
   }
   self.set.inner = function set_inner (selector, stream) {
-    return mixin(self.pipe(set_stream.inner(selector, stream)))
+    return mixin(self.through(set_stream.inner(selector, stream)))
   }
   self.set.attribute = function set_attribute (selector, text) {
-    return mixin(self.pipe(set_stream.attribute(selector, text)))
+    return mixin(self.through(set_stream.attribute(selector, text)))
   }
   self.set.attributes = function set_attributes (selector, obj) {
-    return mixin(self.pipe(set_stream.attributes(selector, obj)))
+    return mixin(self.through(set_stream.attributes(selector, obj)))
   }
   self.replace = function replace (selector, arg) {
     if (h.isFunction(arg)) {
       var s = replace_stream.outer(selector, wrap_callback(arg))
-      return mixin(self.pipe(s))
+      return mixin(self.through(s))
     } else {
       return self.set(selector, arg)
     }
@@ -47,18 +47,18 @@ function mixin (self) {
   self.replace.inner = function replace_inner (selector, arg) {
     if (h.isFunction(arg)) {
       var s = replace_stream.inner(selector, wrap_callback(arg))
-      return mixin(self.pipe(s))
+      return mixin(self.through(s))
     } else {
       return self.set.inner(selector, arg)
     }
   }
   self.replace.attribute = function replace_attribute (selector, attr, cb) {
     var replace_attr = replace_stream.attribute(selector, attr, cb)
-    return mixin(self.pipe(replace_attr))
+    return mixin(self.through(replace_attr))
   }
   self.replace.attributes = function replace_attributes (selector, obj) {
     var replace_attrs = replace_stream.attributes(selector, obj)
-    return mixin(self.pipe(replace_attrs))
+    return mixin(self.through(replace_attrs))
   }
   self.map = function map (callback) {
     return mixin(map_stream(self, function (stream, data) {
@@ -69,12 +69,14 @@ function mixin (self) {
 }
 
 function rheo (text) {
-  var parse = parse_stream()
+  var parse = h.pipeline(function (s) {
+    return s.through(parse_stream())
+  })
   if (arguments.length > 0) {
-    if (h.isString(text)) parse.end(text)
-    else if (h.isUndefined(text)) parse.end()
-    else if (h.isFunction(text.pipe)) parse.pipe(rheo)
-    else if (h.isFunction(text.toString)) parse.end(text.toString())
+    if (h.isString(text)) h([text]).pipe(parse)
+    else if (h.isUndefined(text)) h([]).pipe(parse)
+    else if (h.isFunction(text.pipe)) text.pipe(parse)
+    else if (h.isFunction(text.toString)) h([text.toString()]).pipe(parse)
   }
   return mixin(parse)
 }
@@ -91,6 +93,6 @@ function chain (func) {
 
 function wrap_callback (callback) {
   return function (stream) {
-    return callback(mixin(stream))
+    return callback(mixin(h(stream)))
   }
 }
