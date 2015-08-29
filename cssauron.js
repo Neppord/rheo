4dc1f290309447dfb9d7e46c7b5f9bba60e7f68c
Samuel Ytterbrink <neppord@gmail.com>
2015-08-29 20:10:18 +0200

    Implement css selector on class.

--- cssauron.js
+++ cssauron.js
@@ -1,5 +1,6 @@
 var cssauron = require('cssauron')
 module.exports = cssauron({
   tag: 'name',
-  attr: function (node, attr) {return node.attrs[attr]}
+  attr: function (node, attr) {return node.attrs[attr]},
+  'class': function (node) {return node.attrs['class']}
 })
