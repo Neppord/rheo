b482f74dfff15f17f52e194b5e9a170919c50d95
Erik Sjöstedt <erik.sjoestedt@gmail.com>
2015-11-03 17:57:36 +0100

    Let Rheo support IE-fix comments

--- spec.js
+++ spec.js
@@ -235,6 +235,20 @@ describe('rheo', function () {
     var html_with_deep_comment = '<div><ul><li><!--comment--><p>paragraph</p></li></ul></div>'
     checker(done, html_with_deep_comment)(rheo(html_with_deep_comment).render())
   })
+  it('supports IE-fix comments', function (done) {
+    var html_with_IE_comment = [
+      '<head>',
+      '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->',
+      '<!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->',
+      '<!--[if lt IE 9]>',
+      '  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>',
+      '  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>',
+      '  <link href="/static/css/ie8-fix.css" rel="stylesheet">',
+      '<![endif]-->',
+      '</head>'
+    ].join('\n')
+    checker(done, html_with_IE_comment)(rheo(html_with_IE_comment).render())
+  })
 })
 
 function checker (done, html) {
